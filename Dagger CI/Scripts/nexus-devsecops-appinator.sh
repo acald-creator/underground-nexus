c92a702b72128e9e716eb2c48304083f0dff1a93
Cloud Underground <10262311-Cloud-Underground@users.noreply.gitlab.com>
2025-05-28 06:11:59 +0000

    Update file nexus-devsecops-appinator.sh

--- Dagger CI/Scripts/nexus-devsecops-appinator.sh
+++ Dagger CI/Scripts/nexus-devsecops-appinator.sh
@@ -5,7 +5,7 @@ echo 'docker run -itd --name=nexus-creator-vault -h nexus-creator-vault -p 1050:
 chmod +x /usr/local/bin/DEV
 
 # Create the SEC command script
-echo 'docker run -itd --name=Underground-Nexus -h Underground-Nexus --privileged --init -p 22:22 -p 80:80 -p 8080:8080 -p 443:443 -p 1000:1000 -p 2375:2375 -p 2376:2376 -p 2377:2377 -p 9010:9010 -p 9050:9443 -p 18080:8080 -p 18443:18443 -v /dev:/dev -v underground-nexus-docker-socket:/var/run -v underground-nexus-data:/var/lib/docker/volumes -v nexus-bucket:/nexus-bucket natoascode/underground-nexus:amd64 && docker exec Underground-Nexus bash deploy-olympiad.sh' > /usr/local/bin/SEC
+echo 'docker run -itd --name=Underground-Nexus -h Underground-Nexus --privileged --init -p 22 -p 80:80 -p 8080:8080 -p 443:443 -p 1000:1000 -p 2375:2375 -p 2376:2376 -p 2377:2377 -p 9010:9010 -p 9050:9443 -p 18080:8080 -p 18443:18443 -v /dev:/dev -v underground-nexus-docker-socket:/var/run -v underground-nexus-data:/var/lib/docker/volumes -v nexus-bucket:/nexus-bucket natoascode/underground-nexus:amd64 && docker exec Underground-Nexus bash deploy-olympiad.sh' > /usr/local/bin/SEC
 chmod +x /usr/local/bin/SEC
 
 # Create the OPS command script
